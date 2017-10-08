a4408a4dba3392ebcd96ad53ea9cbc8a6d81926a
ryancor <ryan_cornateanu@yahoo.com>
2017-10-08 09:48:08 -0700

    upload successful

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 import yaml
 import requests
 from flaskext.mysql import MySQL
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, flash, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from services.injections import Injections
 
@@ -69,16 +69,30 @@ def past_runs():
     return render_template('runs.html', data=data)
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    data = ""
+    if request.method == 'POST':
+        file = request.files['file']
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        elif file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        elif file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload',
+                                    filename=filename, uploaded="successful"))
 
-    if request.method == 'GET':
-        data
-    elif request.method == 'POST':
-        data
-
-    return render_template('upload.html', data=data)
+    return render_template('upload.html')
 
 
 @app.route('/', methods=['GET','POST'])
@@ -157,4 +171,8 @@ def fuzz_post():
 	return render_template("results.html", results=data)
 
 if __name__ == '__main__':
-	app.run()
+    app.secret_key = var['SECRET_KEY']
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    app.debug = True
+    app.run()
